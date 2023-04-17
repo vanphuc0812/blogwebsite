2da0742945d2763ea3e6f5dbf904ff55c3d6c591
vanphuc2401 <vanphuc0812@gmail.com>
2023-04-17 14:49:29 +0700

    create project

---
+++ src/test/java/com/example/blogwebsite/BlogWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
