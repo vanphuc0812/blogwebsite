1d618d30caba0975837b7684e2c1a6ddf39f8a30
vanphuc2401 <vanphuc0812@gmail.com>
2023-05-08 13:50:35 +0700

    Config swagger-

---
+++ src/main/java/com/example/blogwebsite/config/OpenApiConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.blogwebsite.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI getOpenApi() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Blog Website")
+                        .description("Operation for Education Purpose")
+                        .version("v1.0")
+                        .license(new License().name("NO LICENSE").url(""))
+                        .contact(new Contact()
+                                .email("vanphuc0812@gmail.com")
+                                .name("Huynh Van Phuc")
+                                .url("")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Spring Documentation")
+                        .url("https://docs.spring.io/spring-framework/docs/current/reference/html/"));
+    }
+}
